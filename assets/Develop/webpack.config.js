4fca9791331014a17a07e321f389b7867a33dc65
Marco Bejarano <marco.bejarano03@gmail.com>
2021-02-21 15:26:12 -0800

    setting up output

---
+++ assets/Develop/webpack.config.js
@@ -0,0 +1,10 @@
+// Setting up requirements
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+// Setting up entry for the app
+const config = {
+    entry: {
+        app: "./"
+    }
+}
