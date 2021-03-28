010c6204b2bbf164c1b784def06da9a7bfbe575a
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-27 23:41:39 -0700

    change location of routes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGOD_URI || "mongodb+srv://new-user:Password12@cl
 });
 
 // routes
-app.use(require("./Develop/routes/api.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
